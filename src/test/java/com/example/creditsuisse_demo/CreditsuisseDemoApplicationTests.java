16a42f2302debef6db3955db0660bca835243525
ranjeetwaje <ranjeet.waje@gmail.com>
2022-05-20 18:02:22 +0530

    intial commit

---
+++ src/test/java/com/example/creditsuisse_demo/CreditsuisseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creditsuisse_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditsuisseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
